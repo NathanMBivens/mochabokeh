8f3f3632a25ba8cdbe8cf713cdbcdc19dc583f05
NathanMBivens <nathanmbivens@gmail.com>
2017-09-11 16:48:52 -0500

    Add files via upload

--- src/js/scrollToPage.js
+++ src/js/scrollToPage.js
@@ -5,8 +5,15 @@ module.exports.scrollToPage = () => {
   $('a[href*="#"]').on('click', function() {
 
     var target = $(this.hash);
-    
+    if (target.length) {
+      event.preventDefault();
 
+      $('html, body').animate({
+        scrollTop: target.offset().top-150
+      }, 1000, function() {
+        var $target = $(target);
 
+      });
+    }
   });
 };
